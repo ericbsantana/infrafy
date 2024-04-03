063db32245780a206088cecce830c8d105b0379b
Eric Santana <bitencourteric@hotmail.com>
2024-04-02 21:43:57 -0300

    feat: add reviews routes

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,29 @@
 import fastify from "fastify";
+import { PrismaClient } from '@prisma/client'
 
+const prisma = new PrismaClient()
 const server = fastify();
 
-server.get("/", async (request, reply) => {
+server.get("/", async (_request, reply) => {
   reply.code(200).header("Content-Type", "text/plain").send("OK");
 });
+
+server.get("/reviews", async (_request, reply) => {
+  const reviews = await prisma.review.findMany()
+  reply.send(reviews)
+})
+
+server.post<{ Body: IReviewBody }>('/reviews', async (request, reply) => {
+  const { text } = request.body
+
+  const post = await prisma.review.create({
+    data: {
+      text
+    },
+  })
+
+  reply.send(post)
+})
 
 server.listen({ host: "0.0.0.0", port: 8080 }, (err, address) => {
   if (err) {
@@ -13,3 +32,5 @@ server.listen({ host: "0.0.0.0", port: 8080 }, (err, address) => {
   }
   console.log(`Server listening at ${address}`);
 });
+
+interface IReviewBody { text: string }
